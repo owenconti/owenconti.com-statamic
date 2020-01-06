d6578baa6c7198b78cb243c15f88e8b2b86d92ff
Owen Conti <owen@ohseemedia.com>
2020-01-05 17:11:49 -0700

    Remove log request middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,8 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \OhSeeSoftware\LaravelServerAnalytics\Http\Middleware\LogRequest::class,
+        \App\Http\Middleware\TrustProxies::class
     ];
 
     /**
