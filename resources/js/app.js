7b3e8531b89c77d4ee6c8e896e91df2f84d3d652
Owen Conti <owen@ohseemedia.com>
2020-01-11 14:48:54 -0700

    Add php for syntax highlighting.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
+import php from 'highlight.js/lib/languages/php';
+
 import Alpine from 'alpinejs';
 import debounce from './debounce';
 
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('php', php);
 hljs.initHighlightingOnLoad();
