1593d46e43f295430a15a5e746d71b1cb65a5906
Owen Conti <owen@ohseemedia.com>
2020-12-01 09:47:43 -0700

    feat: Try generating og images at build time

---
+++ scripts/build-og-images.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const { readdirSync } = require('fs');
+const http = require('http');
+
+const host = 'http://localhost:5000';
+
+async function run() {
+  const browser = await puppeteer.launch();
+  
+  async function generateImage(path) {
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1200,
+      height: 632,
+      deviceScaleFactor: 2,
+    });
+    
+    await page.goto(`${host}/posts/${path}`);
+
+    const title = await page.$eval("head > meta[property='og:title']", element => element.content);
+
+    const imageUrl = `${host}/og-image-generator?title=${Buffer.from(title).toString('base64')}`;
+    await page.goto(imageUrl);
+
+    await page.screenshot({
+      path: `storage/app/static/images/${path}.png`
+    });
+
+    console.log(`Saved ${path} image`);
+  }
+
+  const posts = readdirSync('storage/app/static/posts');
+  for (const post of posts) {
+    if (post === '.' || post === '..') {
+      continue;
+    }
+
+    await generateImage(post);
+  }
+  
+  await browser.close();
+}
+
+Promise.all([
+  run()
+]).then(process.exit);
