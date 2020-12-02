dadda43039e7e8d14cb19d12b4e1566e85c15b09
Owen Conti <owen@ohseemedia.com>
2020-12-01 21:20:36 -0700

    feat: Add newsletter form to site

---
+++ resources/js/NewsletterForm.svelte
@@ -0,0 +1,56 @@
+{#if status === 'success'}
+  <div>
+    <h3 class="text-green heading mb-1">Success!</h3>
+    <p class="text-sm text-gray-600">Thanks for subscribing, you'll receive a confirmation email shortly.</p>
+  </div>
+{:else}
+  {#if status === 'error'}
+    <div>
+      <h3 class="text-red heading mb-1">Error!</h3>
+      <p class="text-sm text-gray-600">{message}</p>
+    </div>
+  {/if}
+
+  <form class="flex justify-between" on:submit|preventDefault="{handleSubmit}">
+    <input type="hidden" name="workspaceId" value="3" />
+    <input type="email" required name="email" placeholder="Email" class="flex-1 border-r-0 rounded-r-none input" />
+    <button type="submit" class="accent-button rounded-l-none">Sign Up</button>
+  </form>
+{/if}
+
+<script>
+  let status;
+  let message;
+    
+  async function handleSubmit(event) {
+    const json = {};
+    new FormData(event.target).forEach((value, key) => {
+      json[key] = value;
+    });
+  
+    const res = await fetch(
+      'https://sendportal.ohseesoftware.com/api/subscription',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(json),
+      }
+    );
+  
+    if (res.ok) {
+      status = 'success';
+    } else {
+      status = 'error';
+      message = 'An error occurred while submitting the form.';
+
+      try {
+        const body = await res.json();
+        message = body && body.message ? body.message : message;
+      } catch (e) {
+        // no op
+      }
+    }
+  }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -9,3 +9,11 @@ import OgImageGenerator from './OgImageGenerator.svelte';
 new OgImageGenerator({
   target: document.getElementById('og-image-generator')
 });
+
+import NewsletterForm from './NewsletterForm.svelte';
+const newsletterElements = document.getElementsByClassName('newsletter-form');
+for (const $el of newsletterElements) {
+  new NewsletterForm({
+    target: $el
+  });
+}
