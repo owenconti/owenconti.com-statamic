df9c64dc5c131da65fbd530e557d3fb156886b9c
Owen Conti <owen@ohseemedia.com>
2021-08-09 23:08:02 -0600

    Fix OG images

--- resources/js/OgImageGenerator.svelte
+++ resources/js/OgImageGenerator.svelte
@@ -1,8 +1,8 @@
 <div class="relative flex items-center justify-center px-4 text-center text-white border-4 og-image bg-brand-dark border-accent">
 	<div>
-		<h1 class="px-2 py-3 text-5xl font-bold text-white bg-accent">{title}</h1>
-		{#if excerpt}
-			<p class="inline-block p-4 mt-8 text-lg text-white border-l-2 border-accent bg-brand-dark-lighten">&ldquo;{excerpt}&rdquo;</p>
+		<h1 class="px-2 py-3 text-5xl font-bold text-white bg-accent">{data.title}</h1>
+		{#if data.excerpt}
+			<p class="inline-block p-4 mt-8 text-lg text-white border-l-2 border-accent bg-brand-dark-lighten">&ldquo;{data.excerpt}&rdquo;</p>
 		{/if}
 	</div>
 	<p class="absolute bottom-0 right-0 mb-4 mr-10 text-4xl tracking-wider font-heading">owenconti.com</p>
@@ -11,13 +11,11 @@
 <script>
 	import { onMount } from 'svelte';
 
-	let title = '';
-	let excerpt = '';
+	let data = '';
 
 	onMount(async () => {
     const params = new URLSearchParams(window.location.search);
-    title = atob(params.get('title'));
-		excerpt = params.get('excerpt') && atob(params.get('excerpt'));
+    data = JSON.parse(atob(params.get('data')));
 	});
 </script>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::statamic('/', 'home', [
-    'title' => 'Home'
+    'title' => 'Web programming articles including tips, tricks, and tutorials on Laravel, PHP, React, Vue, MySQL and more.'
 ]);
